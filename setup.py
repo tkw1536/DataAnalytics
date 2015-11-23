9e7e8e3ab5ed6b0a7692020aa2f26aa09bb26140
Tom Wiesing <tkw01536@gmail.com>
2015-11-23 18:45:15 +0100

    New version 0.12

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
     name = "DataAnalytics",
-    version = "0.0.11",
+    version = "0.0.12",
     author = "Tom Wiesing",
     author_email = "tkw01536@gmail.com",
     description = ("Python Library for DataAnalytics"),
